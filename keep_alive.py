fee2ab706f6985e684922f61818657428c89c58d
Santeri Pohjaranta <49091889+santeri13@users.noreply.github.com>
2026-03-02 16:56:13 +0200

    IP address in secret

--- keep_alive.py
+++ keep_alive.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from threading import Thread
+import os
 
 app = Flask('')
 
@@ -8,7 +9,7 @@ def home():
     return "I'm alive!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host=os.getenv("IP_ADDRESS"), port=8080)
 
 def keep_alive():
     t = Thread(target=run)
